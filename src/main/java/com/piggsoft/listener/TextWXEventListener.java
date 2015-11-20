88e742591a17cdb79d685e2d6eb5a0234c7a1582
pigg <piggsoft@gmail.com>
2015-11-20 14:49:23 +0800

    修改代码

--- src/main/java/com/piggsoft/listener/TextWXEventListener.java
+++ src/main/java/com/piggsoft/listener/TextWXEventListener.java
@@ -1,7 +1,9 @@
 package com.piggsoft.listener;
 
+import com.piggsoft.event.impl.msg.TextMsg;
+
 /**
  * Created by user on 2015/11/16.
  */
-public class TextWXEventListener implements WXEventListener {
+public class TextWXEventListener implements WXEventListener<TextMsg> {
 }
--- src/main/java/com/piggsoft/listener/WXEventListener.java
+++ src/main/java/com/piggsoft/listener/WXEventListener.java
@@ -1,7 +1,9 @@
 package com.piggsoft.listener;
 
+import com.piggsoft.event.WXEvent;
+
 /**
  * Created by user on 2015/11/16.
  */
-public interface WXEventListener {
+public interface WXEventListener<Event extends WXEvent> {
 }
