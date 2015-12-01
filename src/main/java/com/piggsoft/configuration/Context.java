5b1db66ce32de0b5c1a80ba29d92c2ec8cfdb91d
piggsoft <piggsoft@163.com>
2015-12-01 17:21:19 +0800

    当是spring 的 webApplicationContext时，让wxContext变成spring的parentContext，并refreash

--- src/main/java/com/piggsoft/configuration/Context.java
+++ src/main/java/com/piggsoft/configuration/Context.java
@@ -1,6 +1,10 @@
 package com.piggsoft.configuration;
 
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.xml.ResourceEntityResolver;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Properties;
@@ -29,7 +33,57 @@ public class Context {
      * @param configLocations 配置文件地址
      */
     public static void init(String[] configLocations) {
-        CONTEXT = new ClassPathXmlApplicationContext(configLocations, true);
+        init(configLocations, null);
+    }
+
+    /**
+     * 初始化工作
+     *
+     * @param configLocations 配置文件地址
+     * @param parent 父Context
+     */
+    public static void init(String[] configLocations, ApplicationContext parent) {
+        if (null == parent || !(parent instanceof ConfigurableApplicationContext)) {
+            init0(configLocations, parent);
+        } else {
+            initIntoParent(configLocations, parent);
+        }
+        afterInit();
+    }
+
+    /**
+     * 初始化工作
+     *
+     * @param configLocations 配置文件地址
+     * @param parent 父Context
+     */
+    private static void init0(String[] configLocations, ApplicationContext parent) {
+        CONTEXT = new ClassPathXmlApplicationContext(configLocations, true, parent);
+    }
+
+    /**
+     * 初始化工作, 像容器中注入配置，非父子
+     * @param configLocations 配置文件地址
+     * @param context 主容器
+     */
+    private static void initIntoParent(String[] configLocations, ApplicationContext context) {
+        ConfigurableApplicationContext applicationContext = (ConfigurableApplicationContext) context;
+        ApplicationContext current = new ClassPathXmlApplicationContext(configLocations, true);
+        applicationContext.setParent(current);
+        applicationContext.refresh();
+        CONTEXT = context;
+        /*for (String configLocation : configLocations) {
+            XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader((BeanDefinitionRegistry)
+                    applicationContext.getBeanFactory());
+            beanDefinitionReader.setEntityResolver(new ResourceEntityResolver(applicationContext));
+            beanDefinitionReader.loadBeanDefinitions(configLocation);
+        }*/
+    }
+
+    /**
+     * 初始化完成后进行一些工作
+     */
+    private static void afterInit() {
         PROPS = CONTEXT.getBean("properties", Properties.class);
     }
 
